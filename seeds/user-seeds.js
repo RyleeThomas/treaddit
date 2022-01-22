2afafc21d2ee5fdecefdbe68f5e3ff5f7facc273
rylee thomas <ryleetreat@gmail.com>
2022-01-22 01:23:32 -0500

    updates from develop branch

---
+++ seeds/user-seeds.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const faker = require('@faker-js/faker');
+const { User, Trail } = require('../models');
+
+const userdata = [
+    {
+        username: 'Austin Test',
+        email: 'testing@gmail.com',
+        password: 'password123'
+    },
+    {
+        username: `${faker.animal.type()}${faker.commerce.product()}`,
+        email: `${faker.internet.email()}`,
+        password: 'password123'
+
+    }
+
+
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
